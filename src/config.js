9f0dcde0e4348531ac73d6928d85e87e98d9f8ca
GarySPS <layngelngel96@gmail.com>
2025-11-16 16:47:39 +0630

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 //export const MAIN_API_BASE = "/api";
-export const MAIN_API_BASE = "https://novachain2-backend.onrender.com";
+export const MAIN_API_BASE = "https://novachain2-backend.onrender.com/api";
 export const ADMIN_API_BASE = "https://novachain2-admin-backend.onrender.com";
