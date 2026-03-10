1f1a4c154a5ca9f90cd556dbb733792da272efa1
GarySPS <layngelngel96@gmail.com>
2026-03-10 17:49:32 +0630

    wallet

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -4,7 +4,6 @@ import { useAppKit } from '@reown/appkit/react';
 import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { MAIN_API_BASE } from "../config";
-import { useAppKit } from '@reown/appkit/react';
 import { useAccount } from 'wagmi';
 
 export default function SignUpPage() {
